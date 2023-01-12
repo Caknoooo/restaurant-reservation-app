e9a50b24360fb7d3898c23711094bb8735110cd2
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-01-12 19:40:14 +0700

    feat: add new router for Admin middleware

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +27,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', 'admin'])->name('admin.')->prefix('admin')->group(function(){
+    Route::get('/', [AdminController::class, 'index'])->name('index');
 });
 
 require __DIR__.'/auth.php';
