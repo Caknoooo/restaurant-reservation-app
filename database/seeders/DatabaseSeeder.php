92dee25372c1624e403ff48a38469826c3684288
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-01-12 13:36:53 +0700

    feat: add first migrate

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(Admin::class);
     }
 }
