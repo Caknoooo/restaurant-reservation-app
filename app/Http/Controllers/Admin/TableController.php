e42bf7e08717bf3610243523ba44c9ab606a9c82
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-01-16 17:22:30 +0700

    feat: new feature create tables for customer to ordered tables

--- app/Http/Controllers/Admin/TableController.php
+++ app/Http/Controllers/Admin/TableController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\TableStoreRequest;
 use App\Models\Table;
 use Illuminate\Http\Request;
 
@@ -35,9 +36,16 @@ class TableController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(TableStoreRequest $request)
     {
-        //
+      Table::create([
+        'name' => $request->name,
+        'guest_number' => $request->guest_number,
+        'status' => $request->status,
+        'location' => $request->location,
+      ]);
+
+      return to_route('admin.tables.index');
     }
 
     /**
--- app/Models/Table.php
+++ app/Models/Table.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Enums\TableLocation;
+use App\Enums\TableStatus;
 
 class Table extends Model
 {
     use HasFactory;
+
+    protected $fillable =[
+      'name', 'guest_number', 'status', 'location'
+    ];
+
+    protected $casts = [
+      'status' => TableStatus::class,
+      'location' => TableLocation::class
+    ];
 }
