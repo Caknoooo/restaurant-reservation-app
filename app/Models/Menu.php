0d8931edcf6a6a00fadfc87e691045c46ddad437
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-01-16 16:14:41 +0700

    feat: new feature input for menus

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -9,6 +9,10 @@ class Menu extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+      'name', 'image', 'description', 'price'
+    ];
+
     public function categories(){
       return $this->belongsToMany(Category::class, 'category_menu');
     }
