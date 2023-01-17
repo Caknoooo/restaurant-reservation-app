c599aa8ca89c8f3ee3772d08e8f992d762d7d6ae
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-01-17 19:11:52 +0700

    refactor: modified file to synchronize it

--- resources/views/admin/reservations/create.blade.php
+++ resources/views/admin/reservations/create.blade.php
@@ -8,34 +8,94 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="flex m-2 p-2">
-                <a href="{{ route('admin.resevations.index') }}"
-                    class="text-white px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg">Reservations Index</a>
+                <a href="{{ route('admin.reservations.index') }}"
+                    class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Reservation Index</a>
             </div>
-            <div class="p-2 m-2 bg-slate-100 rounded">
+            <div class="m-2 p-2 bg-slate-100 rounded">
                 <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
-                    <form enctype="multipart/form-data">
+                    <form method="POST" action="{{ route('admin.reservations.store') }}">
+                        @csrf
                         <div class="sm:col-span-6">
-                            <label for="title" class="block text-sm font-medium text-gray-700"> Name </label>
+                            <label for="first_name" class="block text-sm font-medium text-gray-700"> First Name </label>
                             <div class="mt-1">
-                                <input type="text" id="title" wire:model.lazy="title" name="title"
+                                <input type="text" id="first_name" name="first_name"
                                     class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                             </div>
+                            @error('first_name')
+                                <div class="text-sm text-red-400">{{ $message }}</div>
+                            @enderror
                         </div>
                         <div class="sm:col-span-6">
-                            <label for="title" class="block text-sm font-medium text-gray-700"> Image </label>
+                            <label for="last_name" class="block text-sm font-medium text-gray-700"> Last Name </label>
                             <div class="mt-1">
-                                <input type="file" id="image" wire:model="newImage" name="image"
+                                <input type="text" id="last_name" name="last_name"
                                     class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                             </div>
+                            @error('last_name')
+                                <div class="text-sm text-red-400">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="sm:col-span-6">
+                            <label for="email" class="block text-sm font-medium text-gray-700"> Email </label>
+                            <div class="mt-1">
+                                <input type="email" id="email" name="email"
+                                    class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                            </div>
+                            @error('email')
+                                <div class="text-sm text-red-400">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="sm:col-span-6">
+                            <label for="tel_number" class="block text-sm font-medium text-gray-700"> Phone Number </label>
+                            <div class="mt-1">
+                                <input type="text" id="tel_number" name="tel_number"
+                                    class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                            </div>
+                            @error('tel_number')
+                                <div class="text-sm text-red-400">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="sm:col-span-6">
+                            <label for="res_date" class="block text-sm font-medium text-gray-700"> Reservation Data </label>
+                            <div class="mt-1">
+                                <input type="datetime-local" id="res_date" name="res_date"
+                                    class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                            </div>
+                            @error('res_date')
+                                <div class="text-sm text-red-400">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        
+                        <div class="sm:col-span-6 pt-5">
+                            <label for="status" class="block text-sm font-medium text-gray-700">Status</label>
+                            <div class="mt-1">
+                                <select id="status" name="status" class="form-multiselect block w-full mt-1">
+                                    @foreach (App\Enums\TableStatus::cases() as $status)
+                                        <option value="{{ $status->value }}">{{ $status->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            @error('status')
+                                <div class="text-sm text-red-400">{{ $message }}</div>
+                            @enderror
                         </div>
                         <div class="sm:col-span-6 pt-5">
-                            <label for="body" class="block text-sm font-medium text-gray-700">Description</label>
+                            <label for="location" class="block text-sm font-medium text-gray-700">Location</label>
                             <div class="mt-1">
-                                <textarea id="body" rows="3" wire:model.lazy="body"
-                                    class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm "></textarea>
+                                <select id="location" name="location" class="form-multiselect block w-full mt-1">
+                                    @foreach (App\Enums\TableLocation::cases() as $location)
+                                        <option value="{{ $location->value }}">{{ $location->name }}</option>
+                                    @endforeach
+                                </select>
                             </div>
+                            @error('location')
+                                <div class="text-sm text-red-400">{{ $message }}</div>
+                            @enderror
                         </div>
-                        <button type="submit" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg mt-4 text-white">Store</button>
+                        <div class="mt-6 p-4">
+                            <button type="submit"
+                                class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Store</button>
+                        </div>
                     </form>
                 </div>
 
--- resources/views/admin/tables/index.blade.php
+++ resources/views/admin/tables/index.blade.php
@@ -1,79 +1,74 @@
 <x-admin-layout>
-  <x-slot name="header">
-      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-          {{ __('Dashboard') }}
-      </h2>
-  </x-slot>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
 
-  <div class="py-12">
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="flex justify-end m-2 p-2">
-          <a href="{{ route('admin.tables.create') }}" class="text-white px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg">New Table</a>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="flex justify-end m-2 p-2">
+                <a href="{{ route('admin.tables.create') }}"
+                    class="text-white px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg">New Table</a>
+            </div>
+            <div class="relative overflow-x-auto">
+                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Name
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Guest
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Status
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Location
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Action
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($tables as $table)
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <td class="px-6 py-4">
+                                    {{ $table->name }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{ $table->guest_number }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{ $table->status->name }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{ $table->location->name }}
+                                </td>
+                                <td scope="row"
+                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    <div class="flex space-x-2">
+                                        <a href="{{ route('admin.tables.edit', $table->id) }}"
+                                            class="px-4 py-2 bg-green-500 hover:bg-green-700 rounded-lg text-white">
+                                            Edit
+                                        </a>
+                                        <form action="{{ route('admin.tables.destroy', $table->id) }}"
+                                            method="POST" onsubmit="return confirm('Are you sure?');"
+                                            class="px-4 py-2 bg-red-500 hover:bg-red-700 rounded-lg text-white">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit">Delete</button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+
+                    </tbody>
+                </table>
+            </div>
         </div>
-        <div class="relative overflow-x-auto">
-          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-              <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                  <tr>
-                      <th scope="col" class="px-6 py-3">
-                          Product name
-                      </th>
-                      <th scope="col" class="px-6 py-3">
-                          Color
-                      </th>
-                      <th scope="col" class="px-6 py-3">
-                          Category
-                      </th>
-                      <th scope="col" class="px-6 py-3">
-                          Price
-                      </th>
-                  </tr>
-              </thead>
-              <tbody>
-                  <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                      <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                          Apple MacBook Pro 17"
-                      </th>
-                      <td class="px-6 py-4">
-                          Sliver
-                      </td>
-                      <td class="px-6 py-4">
-                          Laptop
-                      </td>
-                      <td class="px-6 py-4">
-                          $2999
-                      </td>
-                  </tr>
-                  <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                      <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                          Microsoft Surface Pro
-                      </th>
-                      <td class="px-6 py-4">
-                          White
-                      </td>
-                      <td class="px-6 py-4">
-                          Laptop PC
-                      </td>
-                      <td class="px-6 py-4">
-                          $1999
-                      </td>
-                  </tr>
-                  <tr class="bg-white dark:bg-gray-800">
-                      <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                          Magic Mouse 2
-                      </th>
-                      <td class="px-6 py-4">
-                          Black
-                      </td>
-                      <td class="px-6 py-4">
-                          Accessories
-                      </td>
-                      <td class="px-6 py-4">
-                          $99
-                      </td>
-                  </tr>
-              </tbody>
-          </table>
-        </div>
-      </div>
-  </div>
+    </div>
 </x-admin-layout>
