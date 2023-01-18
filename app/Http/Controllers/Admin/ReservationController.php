62858028ee2c78a99a24f55bbd19921b57aa35e3
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-01-18 20:48:41 +0700

    feat: add table guest date detection conflict and fixed it

--- app/Http/Controllers/Admin/ReservationController.php
+++ app/Http/Controllers/Admin/ReservationController.php
@@ -30,7 +30,7 @@ class ReservationController extends Controller
      */
     public function create()
     {
-      $tables = Table::all();
+      $tables = Table::where('status', TableStatus::Available)->get();
       return view('admin.reservations.create', compact('tables'));
     }
 
@@ -42,6 +42,17 @@ class ReservationController extends Controller
      */
     public function store(ReservationStoreRequest $request)
     {
+      $table = Table::findOrFail($request->table_id);
+      if($request->guest_number > $table->guest_number){
+        return back()->with('warning', 'Please choose the table base on guests.');
+      }
+      $request_date = Carbon::parse($request->res_date);
+      foreach($table->reservations as $res){
+        if($res->res_date->format('Y-m-d')  == $request_date->format('Y-m-d')){
+          return back()->with('warning', 'This table is reserved for this date.');
+        }
+      }
+
       Reservation::create($request->validated());
 
       return to_route('admin.reservations.index')->with('success', 'Reservation created successfully');
