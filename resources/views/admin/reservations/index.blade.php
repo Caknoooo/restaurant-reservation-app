51e41cd766af4b5532661e913238e94367b24ca7
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-01-18 20:23:54 +0700

    refactor: modify reservations and add flash message UI for admin

--- resources/views/admin/reservations/index.blade.php
+++ resources/views/admin/reservations/index.blade.php
@@ -9,59 +9,86 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-end m-2 p-2">
                 <a href="{{ route('admin.reservations.create') }}"
-                    class="text-white px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg">New Reservation</a>
+                    class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">New Reservation</a>
             </div>
-            <div class="relative overflow-x-auto">
-                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                        <tr>
-                            <th scope="col" class="px-6 py-3">
-                                Name
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                Email
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                Phone Number
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                Reservation Data
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                Table
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                Guests
-                            </th>
-                            
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($reservations as $reservation)
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                <td class="px-6 py-4">
-                                    {{ $reservation->first_name }} {{ $reservation->last_name }}
-                                </td>
-                                <td class="px-6 py-4">
-                                    {{ $reservation->email }}
-                                </td>
-                                <td class="px-6 py-4">
-                                    {{ $reservation->tel_number }}
-                                </td>
-                                <td class="px-6 py-4">
-                                    {{ $reservation->res_date }}
-                                </td>
-                                <td class="px-6 py-4">
-                                    {{ $reservation->table->name }}
-                                </td>
-                                <td class="px-6 py-4">
-                                    {{ $reservation->guest_number }}
-                                </td>
-                            </tr>
-                        @endforeach
+            <div class="flex flex-col">
+                <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="inline-block py-2 min-w-full sm:px-6 lg:px-8">
+                        <div class="overflow-hidden shadow-md sm:rounded-lg">
+                            <table class="min-w-full">
+                                <thead class="bg-gray-50 dark:bg-gray-700">
+                                    <tr>
+                                        <th scope="col"
+                                            class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                            Name
+                                        </th>
+                                        <th scope="col"
+                                            class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                            Email
+                                        </th>
+                                        <th scope="col"
+                                            class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                            Date
+                                        </th>
+                                        <th scope="col"
+                                            class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                            Table
+                                        </th>
+                                        <th scope="col"
+                                            class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                            Guests
+                                        </th>
+                                        <th scope="col" class="relative py-3 px-6">
+                                            <span class="sr-only">Edit</span>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($reservations as $reservation)
+                                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                            <td
+                                                class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                {{ $reservation->first_name }} {{ $reservation->last_name }}
+                                            </td>
+                                            <td
+                                                class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                                                {{ $reservation->email }}
+                                            </td>
+                                            <td
+                                                class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                                                {{ $reservation->res_date }}
+                                            </td>
+                                            <td
+                                                class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                                                {{ $reservation->table->name }}
+                                            </td>
+                                            <td
+                                                class="py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400">
+                                                {{ $reservation->guest_number }}
+                                            </td>
+                                            <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
+                                                <div class="flex space-x-2">
+                                                    <a href="{{ route('admin.reservations.edit', $reservation->id) }}"
+                                                        class="px-4 py-2 bg-green-500 hover:bg-green-700 rounded-lg  text-white">Edit</a>
+                                                    <form
+                                                        class="px-4 py-2 bg-red-500 hover:bg-red-700 rounded-lg text-white"
+                                                        method="POST"
+                                                        action="{{ route('admin.reservations.destroy', $reservation->id) }}"
+                                                        onsubmit="return confirm('Are you sure?');">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit">Delete</button>
+                                                    </form>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
 
-                    </tbody>
-                </table>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -85,6 +85,26 @@
             </nav>
         </div>
         <main class="m-2 p-8 w-full">
+            <div>
+                @if (session()->has('danger'))
+                    <div class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-gray-800 dark:text-red-400"
+                        role="alert">
+                        <span class="font-medium">{{ session()->get('danger') }}</span>
+                    </div>
+                @endif
+                @if (session()->has('success'))
+                    <div class="p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-gray-800 dark:text-green-400"
+                        role="alert">
+                        <span class="font-medium">{{ session()->get('success') }}</span>
+                    </div>
+                @endif
+                @if (session()->has('warning'))
+                    <div class="p-4 mb-4 text-sm text-yellow-700 bg-yellow-100 rounded-lg dark:bg-gray-800 dark:text-yellow-300"
+                        role="alert">
+                        <span class="font-medium">{{ session()->get('warning') }}</span>
+                    </div>
+                @endif
+            </div>
             {{ $slot }}
         </main>
     </div>
