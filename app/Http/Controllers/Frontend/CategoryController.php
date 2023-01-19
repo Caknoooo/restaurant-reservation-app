5da1f3a842576bd4c9223e52890cf05d759bd86b
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-01-19 17:30:42 +0700

    feat: add empty file controller for menu, category, and reservation

---
+++ app/Http/Controllers/Frontend/CategoryController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+  public function index(){
+    return 'category';
+  }
+}
