e12a885103a6d82a8bef8a2e4239d51977a6c3db
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-01-21 09:01:30 +0700

    feat: add controller for categories page and showing the categories

--- app/Http/Controllers/Frontend/CategoryController.php
+++ app/Http/Controllers/Frontend/CategoryController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
   public function index(){
-    return 'category';
+    $categories = Category::all();
+    return view('categories.index', compact('categories'));
+  }
+
+  public function show(Category $category){
+    return view('categories.show', compact('category'));
   }
 }
