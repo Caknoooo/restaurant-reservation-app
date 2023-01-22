6560e1869fd7e531087c632a026341ce678fc80a
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-01-23 05:57:01 +0700

    fix: fixed routes profile.edit is missing

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,12 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
 Route::middleware(['auth', 'admin'])->name('admin.')->prefix('admin')->group(function () {
     Route::get('/', [AdminController::class, 'index'])->name('index');
     Route::resource('/categories', CategoryController::class);
